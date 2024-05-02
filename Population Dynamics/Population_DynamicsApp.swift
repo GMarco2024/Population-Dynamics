2bc5a205624ef4e03bbd40b384eea883d2dccf0c
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-05-02 13:33:40 -0500

    Initial Commit

---
+++ Population Dynamics/Population_DynamicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Population_DynamicsApp.swift
+//  Population Dynamics
+//
+//  Created by Marco Gonzalez on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Population_DynamicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
