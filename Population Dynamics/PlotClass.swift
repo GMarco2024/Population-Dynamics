b0391037baf1b44a72496da447648354747fd8d5
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-05-02 19:46:07 -0500

    Comments were made.

--- Population Dynamics/PlotClass.swift
+++ Population Dynamics/PlotClass.swift
@@ -6,16 +6,18 @@
 //  Modified by Marco Gonzalez 2/11/24
 //
 
-
 import Foundation
 import SwiftUI
 
 class PlotClass: ObservableObject {
     
+    // Array of PlotDataClass instances. This changes to this array will update any SwiftUI views observing this object.
     @Published var plotArray: [PlotDataClass]
 
     init() {
+        // Creates an initial PlotDataClass instance configured to start with a blank plot.
         let initialPlot = PlotDataClass(fromLine: true)
+        // Initializes the plotArray with two instances of the same initial plot. This could be for showing multiple views or states of the same data set in some form.
         self.plotArray = [initialPlot, initialPlot]
     }
 }
--- Population Dynamics/PlotDataClass.swift
+++ Population Dynamics/PlotDataClass.swift
@@ -3,31 +3,37 @@
 //  Bifurcation Diagram
 //
 //  Created by Jeff_Terry on 1/15/24.
-//  Modified by Marco Gonzalez on [your modification date]
+//  Modified by Marco Gonzalez on 05/2/2024
 //
 
 import Foundation
 import SwiftUI
 import Observation
 
+// Observable class that manages the data and settings for plotting diagrams.
 @Observable class PlotDataClass {
     
+    // Holds the actual plot data points as an array of PlotDataStruct.
     @MainActor var plotData = [PlotDataStruct]()
+    // Manages settings for plot appearance such as axis labels, limits, and colors.
     @MainActor var changingPlotParameters: ChangingPlotParameters = ChangingPlotParameters()
+    // This holds text that used for calculation
     @MainActor var calculatedText = ""
+    // This tracks the number of points plotted.
     @MainActor var pointNumber = 1.0
     
+    // Initializes the class with an option to start with a blank plot.
     init(fromLine line: Bool) {
         Task {
             await self.plotBlank()
         }
     }
 
-    /// Displays a Blank Chart
+    /// Initializes a blank chart with predefined parameters, effectively resetting the plot.
     @MainActor func plotBlank() {
-        zeroData()
+        zeroData()  // Clears any existing data.
 
-        // Set the Plot Parameters
+    // Set the Plot Parameters with initial values for a blank plot.
         changingPlotParameters.yMax = 1.0
         changingPlotParameters.yMin = 0.0
         changingPlotParameters.xMax = 30.0
@@ -39,22 +45,22 @@ import Observation
         changingPlotParameters.title = "Initial Plot"
     }
 
-    /// Zeros Out The Data Being Plotted
+    /// Clears all data points and resets the point counter and calculated text.
     @MainActor func zeroData() {
-        plotData = []
-        pointNumber = 1.0
-        calculatedText = "" // Ensure text is cleared when data is zeroed
+        plotData = []  // Empty the data array.
+        pointNumber = 1.0  // Reset the point counter.
+        calculatedText = ""  // Clear any text associated with calculations.
     }
 
-    /// Append Data appends Data to the Plot. Increments the pointNumber for 1-D Data
-    /// - Parameter dataPoint: Array of (x, y) data for plotting
+    /// Adds an array of data points to the plot and updates the plot count and display text.
+    /// - Parameter dataPoint: Array of (x, y) data for plotting.
     @MainActor func appendData(dataPoint: [(x: Double, y: Double)]) {
         for item in dataPoint {
             let dataValue: [PlotDataStruct] = [.init(xVal: item.x, yVal: item.y)]
             plotData.append(contentsOf: dataValue)
             pointNumber += 1.0
 
-            // Update calculatedText to include new point
+    // This updates the calculatedText to include new point details for display.
             calculatedText += "\(item.x), \(item.y)\n"
         }
     }
